cd679b0a08599b6fb1466d7c35d547a0289b395d
Michael Pirotte <michael.pirotte@imaios.com>
2021-04-01 16:41:02 +0200

    Initial Commit

---
+++ testInsertions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testInsertions
+//
+//  Created by Michael Pirotte 3 on 01/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
