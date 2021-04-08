976e2001c4477aee10454aa2ded11672692f2f1c
Crabman <michael.pirotte@gmail.com>
2021-04-08 11:41:16 +0200

    Added an insertion with prepared statement

--- testInsertions/ViewController.swift
+++ testInsertions/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
             config.prepareDatabase { db in
 //                try db.usePassphrase("test")
 //                try db.execute(sql: "PRAGMA cipher_page_size = 16384;")
-//                try db.execute(sql: "PRAGMA cipher_memory_security = OFF;")
+                try db.execute(sql: "PRAGMA cipher_memory_security = OFF;")
             }
             let dbQueue = try DatabaseQueue(path: fileURL.path, configuration: config)
             testDatabase = dbQueue
@@ -45,8 +45,55 @@ class ViewController: UIViewController {
             
         }
     }
-    
+    func performInsertionsWithStatement(_ nbOfInserts:Int) {
+        
+        do{
+            let start = DispatchTime.now() // <<<<<<<<<< Start time
+            try testDatabase!.inTransaction { db in
+                
+                try db.drop(table: "SimpleTable")
+                
+                let creationRequest = """
+                CREATE TABLE "SimpleTable" (
+                    "_id"    INTEGER,
+                    "value1"    INTEGER,
+                    "value2"    INTEGER,
+                    "value3"    INTEGER,
+                    "value4"    TEXT,
+                    "value5"    INTEGER,
+                    "value6"    INTEGER,
+                    "value7"    INTEGER
+                )
+                """
+                try db.execute(sql: creationRequest)
+                
+                let statement = try db.makeUpdateStatement(sql: """
+                                                        INSERT INTO SimpleTable ('_id', 'value1','value2','value3','value4','value5','value6','value7') VALUES (?,?,?,?,?,?,?,?)
+                                        """)
+                var i=0
+                while i < nbOfInserts {
+                    statement.setUncheckedArguments([i,1,2,3,"text",4,5,6,7])
+                    try statement.execute()
+                    i+=1
+                }
+                return .commit
+            }
+            let end = DispatchTime.now()
+                        let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
+                            let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
+
+            print("total time to insert \(nbOfInserts) items : \(timeInterval) seconds")
+            
+        
+            
+        } catch let error {
+            print(error)
+        }
+    }
     func performInsertions(_ nbOfInserts:Int) {
+        
+        //let elements =
+        
         do{
             //print("Inserts started")
             let start = DispatchTime.now() // <<<<<<<<<< Start time
@@ -80,7 +127,8 @@ class ViewController: UIViewController {
                     request.removeLast()//removes last ","
                 }
                 request.append(";")
-                try db.execute(sql: request)
+                try db.execute(sql: request)            
+            
                 return .commit
             }
             let end = DispatchTime.now()
@@ -103,6 +151,9 @@ class ViewController: UIViewController {
         performInsertions(1000000)
     }
     
+    @IBAction func insert1millionStatementTap(_ sender: Any) {
+        performInsertionsWithStatement(1000000)
+    }
     @IBAction func insertButtonTap(_ sender: Any) {
         let nbOfInserts = Int(nbInsertsTextField.text ?? "1")
         performInsertions(nbOfInserts!)
